7a3ef6ba0806671a8d14f183672a2510e6b74abb
ryfh <nonhung@aliyun.com>
2019-03-04 14:10:34 +0800

    init

---
+++ src/main/java/com/example/mongodb_nonhung/demoss/MongodbNonhungApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodb_nonhung.demoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbNonhungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbNonhungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mongodb_nonhung/demoss/DemossApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.mongodb_nonhung.demoss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemossApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
